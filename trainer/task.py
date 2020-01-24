3a7645f955503a293e97515bef54f10189ed0099
juan97serrano <jjserrano97@gmail.com>
2020-01-24 19:06:57 +0100

    trainer

--- trainer/task.py
+++ trainer/task.py
@@ -1,6 +1,10 @@
 """A simple main file to showcase the template."""
 
+import argparse
 import logging.config
+
+
+from google.cloud import storage
 
 """
 This module is an example for a single Python application with some
@@ -12,75 +16,58 @@ template. Please feel free to remove this, or the other
 (sklearn_main.py), or adapt as you need.
 """
 
+LOGGER = logging.getLogger()
 
-def add(x, y):
-    """Add the given parameters.
+def _list_files_by_prefix(bucket_name,prefix):
 
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the sum of x and y
-    :rtype: int
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(
+        bucket_name, prefix=prefix, delimiter=None
+    )
+    names = [blob.name for blob in blobs]
+    return names
+
+
+def _download_file(bucket_name, remote_name, dest_name):
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(remote_name)
+    blob.download_to_filename(dest_name)
+
+    print(
+        "Blob {} downloaded to {}.".format(
+            remote_name, dest_name
+        )
+    )
+
+
+def download_prepare_data(bucket_name, prefix):
+    """Download and prepare the data for training
+
+    Args:
+      bucket_name: Name of the bucket
+      prefix: prefix to the bathc
     """
-    return x + y
+    _list_files_by_prefix(bucket_name, prefix)
 
 
-def subtract(x, y):
-    """Substract the given parameters.
 
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the diff of x and y
-    :rtype: int
-    """
-    return x - y
-
-
-def multiply(x, y):
-    """Multiply the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the multiplication of x and y
-    :rtype: int
-    """
-    return x * y
-
-
-def divide(x, y):
-    """Divide the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the division of x by y
-    :rtype: int
-    """
-    return x / y
-
-
-def main():
-    """Entry point for your module."""
-    logger = logging.getLogger()
-    x1 = 2
-    y1 = 3
-    logger.info('Realizando suma')
-    logger.debug('{x} + {y} = '.format(x=x1, y=y1) + str(add(x1, y1)))
-    logger.info('Realizando resta')
-    logger.debug('{x} - {y} = '.format(x=x1, y=y1) + str(subtract(x1, y1)))
-    logger.info('Realizando multiplicación')
-    logger.debug('{x} * {y} = '.format(x=x1, y=y1) + str(multiply(x1, y1)))
-    logger.info('Realizando división')
-    logger.debug('{x} / {y} = '.format(x=x1, y=y1) + str(divide(x1, y1)))
-    logger.debug('TERMINADO')
-    logger.critical('FAIL')
-
+def train_and_evaluate(bucket_name, prefix):
+    download_prepare_data(bucket_name, prefix)
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bucket-name", required=True)
+    parser.add_argument("--prefix", required=True)
+
+    args = parser.parse_args()
+
+    bucket_name = args.bucket_name
+    prefix = args.prefix
+
+    train_and_evaluate(bucket_name, prefix)
