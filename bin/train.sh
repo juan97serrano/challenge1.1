74bb72313f6f66ab9a8e3a4d88f17b17d1b18ff4
juan97serrano <jjserrano97@gmail.com>
2020-01-25 09:51:01 +0100

    add bin

---
+++ bin/train.sh
@@ -0,0 +1,15 @@
+gcloud ai-platform jobs submit training generator_train_`date +"%s"` \
+  --python-version=3.7 \
+  --runtime-version=1.15 \
+  --scale-tier basic \
+  --package-path ./trainer \
+  --module-name trainer.task \
+  --job-dir=gs://jj-challenge1/tmp/ \
+  --region us-central1 \
+  -- \
+  --epochs 10 \
+  --download \
+  --bucket-name kschool-challenge-vcm \
+  --prefix data/dogs_cats/
+ 
+
